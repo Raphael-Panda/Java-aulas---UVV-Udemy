6a4c41db85ae1e5b812cc67e6eb9f0f75a67d293
Raphael <74225095+Raphael-Panda@users.noreply.github.com>
2026-08-17 10:52:44 -0300

    UVV - Aula 1 de POO

---
+++ Aula01.java
@@ -0,0 +1,39 @@
+void  main(){
+      
+    /* int idade = 18;
+    var ch = 'a'; // var detecta a variavel de acordo com o valor que é colocado
+    
+    if(idade >= 18){
+        System.out.println("Pode entrar no buteco 01");
+    }
+    else{
+        System.out.println("Não pode entrar no buteco 01");
+    }
+ 
+   // if feito de outra maneira
+   var mensagem = idade > 18
+           ? "Pode entrar no buteco 02"
+           : "Não pode entrar no buteco 02";
+           
+   System.out.println(mensagem);
+   
+
+    */
+   
+    int ns = 10;
+    var chute = 0;
+    
+    while(chute != ns){
+       // fazer print e o scanf, professor usou InOut mas não estava indo
+    }
+    
+    
+    if(chute == ns){
+        System.out.println("Você ganhou!!");
+    }
+    else{
+        System.out.println("Você perdeu AHAHAHAHAHAHAH");
+    }
+   
+   
+}
