18bb40590e342be0c1d5963ba7f2966459a114fa
Raphael <74225095+Raphael-Panda@users.noreply.github.com>
2026-09-25 10:49:07 -0300

    guerreiro em processo

--- Guerreiro.java
+++ Guerreiro.java
@@ -2,20 +2,35 @@ public class Guerreiro {
     
     String nome;
     Bolsa minhaBolsa;
-
+    int qtdVidas;
+    String classe;
+    
     public Guerreiro(String nome) {
         this.nome = nome;
+        this.minhaBolsa = new Bolsa();
     }
     
-    
     String vidaExtra(){
-        //return vidaExtra;
+        String pedido = InOut.leString("Peça misericordia ao Oraculo por 1 vida extra: ");
+        return pedido;
     }
     
     int sortearVidas(){
-        int QtdVidas;
-        QtdVidas = (int) (Math.random() * (12 - 9 + 1)) + 9;
-        return QtdVidas;
+        qtdVidas = (int) (Math.random() * (12 - 9 + 1)) + 9;
+        this.qtdVidas = qtdVidas;
+        return qtdVidas;
+    }
+    
+    String definirClasse(String classeEscolhida){
+        InOut.MsgDeInformacao(
+        "Escolha uma Classe de Guerreiro",
+        "1 - Paladino\n 2 - Berserker\n 3 - Samurai"    
+        );   
+        String classeEscolhida = InOut.leString("Digite o nome da classe escolhida: ");
+        
+        if(classe.equalsIgnoreCase(classeEscolhida))
+        
+        return classe;
     }
     
 }
